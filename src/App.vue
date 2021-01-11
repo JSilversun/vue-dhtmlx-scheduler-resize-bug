34814ba28e790ba5c11a77d4b240acda58f206cf
Julia Suarez <js@plusteam.tech>
2021-01-11 16:29:34 -0400

    feat: Finish demo reproduction

--- src/App.vue
+++ src/App.vue
@@ -2,59 +2,53 @@
   <v-app>
     <v-app-bar
       app
+      clipped-left
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-app-bar-nav-icon @click="isMini = !isMini" />
     </v-app-bar>
+    <v-navigation-drawer clipped permanent app :mini-variant="isMini">
+      <v-list>
+        <v-list-item v-for="{title, to, icon} in links" :key="title" :to="to">
+          <v-list-item-action>
+            <v-icon>mdi-{{icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{title}}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
-      <HelloWorld/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data: () => ({
-    //
+    isMini: false,
+    links: [
+      {
+        title: 'Page 1',
+        icon: 'poll-box',
+        to: '/page1'
+      },
+      {
+        title: 'Timeline',
+        icon: 'folder',
+        to: '/timeline'
+      }
+    ]
   }),
 };
 </script>
---
+++ src/views/Timeline.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-container fluid class="timeline-wrapper pa-2 grey darken-1">
+    <div id="scheduler" style="width: 100%; height:800px"></div>
+  </v-container>
+</template>
+<script>
+/* eslint-disable no-undef */
+import '../scheduler_trial/codebase/dhtmlxscheduler';
+import '../scheduler_trial/codebase/ext/dhtmlxscheduler_timeline';
+import '../scheduler_trial/codebase/ext/dhtmlxscheduler_container_autoresize';
+
+  function initScheduler() {
+    const sections=[
+      {key:1, label:"James Smith"},
+      {key:2, label:"John Williams"},
+      {key:3, label:"David Miller"},
+      {key:4, label:"Linda Brown"}
+    ];
+
+    scheduler.createTimelineView({
+      name:	"timeline",
+      x_unit:	"minute",
+      x_date:	"%H:%i",
+      x_step:	30,
+      x_size: 24,
+      x_start: 16,
+      x_length:	48,
+      y_unit:	sections,
+      y_property:	"section_id",
+      render:"bar"
+    });
+
+    scheduler.init('scheduler',new Date(2017,5,30),"timeline");
+    scheduler.parse([
+      { start_date: "2017-06-30 09:00", end_date: "2017-06-30 12:00", text:"Task A-12458", section_id:1},
+      { start_date: "2017-06-30 10:00", end_date: "2017-06-30 16:00", text:"Task A-89411", section_id:1},
+      { start_date: "2017-06-30 10:00", end_date: "2017-06-30 14:00", text:"Task A-64168", section_id:1},
+      { start_date: "2017-06-30 16:00", end_date: "2017-06-30 17:00", text:"Task A-46598", section_id:1},
+
+      { start_date: "2017-06-30 12:00", end_date: "2017-06-30 20:00", text:"Task B-48865", section_id:2},
+      { start_date: "2017-06-30 14:00", end_date: "2017-06-30 16:00", text:"Task B-44864", section_id:2},
+      { start_date: "2017-06-30 16:30", end_date: "2017-06-30 18:00", text:"Task B-46558", section_id:2},
+      { start_date: "2017-06-30 18:30", end_date: "2017-06-30 20:00", text:"Task B-45564", section_id:2},
+
+      { start_date: "2017-06-30 08:00", end_date: "2017-06-30 12:00", text:"Task C-32421", section_id:3},
+      { start_date: "2017-06-30 14:30", end_date: "2017-06-30 16:45", text:"Task C-14244", section_id:3},
+
+      { start_date: "2017-06-30 09:20", end_date: "2017-06-30 12:20", text:"Task D-52688", section_id:4},
+      { start_date: "2017-06-30 11:40", end_date: "2017-06-30 16:30", text:"Task D-46588", section_id:4},
+      { start_date: "2017-06-30 12:00", end_date: "2017-06-30 18:00", text:"Task D-12458", section_id:4}
+    ]);
+  }
+  export default {
+    name: 'TimelinePage',
+    async mounted() {
+      initScheduler();
+    },
+  }
+</script>
+<style>
+  @import "~@/scheduler_trial/codebase/dhtmlxscheduler_material.css";
+</style>
